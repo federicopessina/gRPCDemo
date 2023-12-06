88ec4a179e884b1e645a3010cf54e893fd06e6be
Pessina Federico (UniCredit) <FEDERICO.PESSINA@unicredit.eu>
2023-12-06 14:30:24 +0100

    Add project files.

---
+++ gRPCServer/Services/CustomersService.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using gRPCServer.Protos;
+
+namespace gRPCServer.Services;
+
+public class CustomersService : Customer.CustomerBase
+{
+    private readonly ILogger<CustomersService> logger;
+
+    public CustomersService(ILogger<CustomersService> logger)
+    {
+        this.logger = logger;
+    }
+
+    public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+    {
+        CustomerModel output = new CustomerModel();
+
+        if (request.UserId == 1)
+        {
+            output.FirstName = "Jamie";
+            output.LastName = "Smith";
+        }
+        else if (request.UserId == 2)
+        {
+            output.FirstName = "Jane";
+            output.LastName = "Doe";
+        }
+        else
+        {
+            output.FirstName = "Greg";
+            output.LastName = "Thomas";
+        }
+
+        return Task.FromResult(output);
+    }
+
+    public override async Task GetNewCustomers(
+        NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+
+        foreach (var cust in customers)
+        {
+            await Task.Delay(1000);
+            await responseStream.WriteAsync(cust);
+        }
+    }
+}
---
+++ gRPClient/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using gRPCServer;
+using gRPCServer.Protos;
+
+Console.WriteLine("Hello, gRPC!");
+
+//var input = new HelloRequest { Name = "Tim" };
+//var channel = GrpcChannel.ForAddress("http://localhost:5146");
+
+//// NOTE Add using but not the reference.
+//// Acts like a local call.
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(input);
+
+//Console.WriteLine(reply.Message);
+
+var channel = GrpcChannel.ForAddress("http://localhost:5146");
+var customerClient = new Customer.CustomerClient(channel);
+var clientRequested = new CustomerLookupModel { UserId = 2 };
+var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+Console.WriteLine($"{ customer.FirstName } { customer.LastName }");
+
+
+Console.WriteLine("New Customer List");
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+
+        Console.WriteLine($"{ currentCustomer.FirstName } { currentCustomer.LastName }: { currentCustomer.EmailAddress }");
+    }
+}
+
+
+Console.ReadLine();
+
+
